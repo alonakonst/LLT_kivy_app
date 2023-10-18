34de8ad89122e64582aa45c1ab8e30cf7cd763cc
Alona Konstantinova <alona@ruc.dk>
2023-10-18 10:52:22 +0200

    Initial

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button

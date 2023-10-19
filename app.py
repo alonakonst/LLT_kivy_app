2adc3a4a9f4d74b09f85f722cf13f58af2393fbb
Alona Konstantinova <alona@ruc.dk>
2023-10-19 15:10:08 +0200

    initial setup of the database

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+import sqlite3
 
 class WelcomeWindow(Screen):
    user_name = ObjectProperty(None)
@@ -16,17 +17,48 @@ class WelcomeWindow(Screen):
        print("Name:", self.user_name.text)
        self.user_name.text = ""
 
+
+
 class SecondWindow(Screen):
     pass
 
 class InsertIntoDictionary(Screen):
     def submit_new_word(self):
-        print("New word:", self.new_word.text)
-        self.new_word.text = ""
+        con = sqlite3.connect('dictionary.db')
+        cur = con.cursor()
+        cur.execute("INSERT INTO words VALUES(:first)",
+{
+                'first': self.new_word.text,
+            })
+        con.commit()
+        con.close()
 
+    def show_dictionary(self):
+        con = sqlite3.connect('dictionary.db')
+        cur = con.cursor()
+        cur.execute("SELECT * FROM words")
+        records = cur.fetchall()
+        word = ''
+        for i in records:
+            word = f'{word}\n{i[0]}'
+            self.manager.get_screen("dictionary").ids.word_label.text = f'{word}'
+        con.commit()
+        con.close()
+
+class DisplayDictionary(Screen):
+    pass
 class WindowManager(ScreenManager):
     pass
 
+con = sqlite3.connect('dictionary.db')
+cur = con.cursor()
+cur.execute("""CREATE TABLE if not exists words(
+        word text
+        translation text
+        notes text)
+    """)
+con.commit()
+con.close()
 
 kv = Builder.load_file("style.kv")
 

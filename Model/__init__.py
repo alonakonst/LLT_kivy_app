dcf2c9679615e762306a21ceb8cc6cd249913f3b
Robert Spralja <robert.spralja@gmail.com>
2023-10-25 19:42:11 +0200

    feat: intialise_database utility function

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,1 +1,3 @@
+from .database import database
+
 from .dictionary_entry import DictionaryEntry
---
+++ Utility/initialise_database.py
@@ -0,0 +1,18 @@
+"""
+Create all the tables in the database using initialise_database
+Usage:
+    All Models that are stored in the database should be imported and included in the TABLES array (as classes)
+"""
+
+from Model import database
+
+from Model import DictionaryEntry
+
+TABLES = [
+    DictionaryEntry,
+]
+
+
+def initialise_database():
+    with database:
+        database.create_tables(TABLES)

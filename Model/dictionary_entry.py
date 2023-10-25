f9f7ef1fa824a70248b2fa36543dae0a8f6ad32e
Robert Spralja <robert.spralja@gmail.com>
2023-10-25 21:40:11 +0200

    feat: added __str__ method to DictionaryEntry

--- Model/dictionary_entry.py
+++ Model/dictionary_entry.py
@@ -7,3 +7,6 @@ class DictionaryEntry(Base):
     text = TextField(null=False)
     translation = TextField(null=True)
     notes = TextField(null=True)
+
+    def __str__(self) -> str:
+        return f"{{id={self.id} text={self.text!r} translation={self.translation!r} notes={self.notes!r}}}"

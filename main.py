7fb79277ee344149ddac689953eda58a3470d64a
Robert Spralja <robert.spralja@gmail.com>
2023-10-29 17:15:35 +0100

    chore: removed unused imports from main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import kivy
 from kivy.app import App
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-from kivy.uix.floatlayout import FloatLayout
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.core.window import Window
-import sqlite3
 from Controller.main_screen import MainScreenController
 from Model.main_screen import MainScreenModel
 

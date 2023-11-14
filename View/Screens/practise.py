87d7b32ee4390cbe3d0acbc328353c17c4f8f5c1
Robert Spralja <robert.spralja@gmail.com>
2023-11-14 16:59:48 +0100

    feat: practise view - 1st prototype

--- View/Screens/practise.py
+++ View/Screens/practise.py
@@ -4,7 +4,17 @@ from kivy.lang import Builder
 
 
 class Practise(Screen):
-    pass
+    def answer0(self):
+        pass
+
+    def answer1(self):
+        pass
+
+    def answer2(self):
+        pass
+
+    def answer3(self):
+        pass
 
 
 Builder.load_file(os.path.join(os.path.dirname(__file__), "practise.kv"))

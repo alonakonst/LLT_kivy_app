d6c9793df280e8a19f08f05028aaa1971e80e62f
Robert Spralja <robert.spralja@gmail.com>
2023-11-14 18:14:23 +0100

    feat: Practise Controller

---
+++ Controller/__init__.py
@@ -0,0 +1,1 @@
+from .practice_controller import PracticeController
---
+++ Controller/practice_controller.py
@@ -0,0 +1,9 @@
+from Model import Quiz
+
+
+class PracticeController:
+
+    def generate_quiz(self):
+        """dummy"""
+
+        return Quiz("Question?", ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'])

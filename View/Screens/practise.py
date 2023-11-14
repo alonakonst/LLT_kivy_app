9b742c5269371933e16d6d038a109988525859aa
Robert Spralja <robert.spralja@gmail.com>
2023-11-14 13:42:18 +0100

    chore: formatting

--- View/Screens/practise.py
+++ View/Screens/practise.py
@@ -1,7 +1,10 @@
 from kivy.uix.screenmanager import Screen
 import os
 from kivy.lang import Builder
+
+
 class Practise(Screen):
     pass
+
 
 Builder.load_file(os.path.join(os.path.dirname(__file__), "practise.kv"))

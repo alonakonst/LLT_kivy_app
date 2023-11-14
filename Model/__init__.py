697bc3c78648bfedbd7dc88d2ea87529bd4ac21a
Robert Spralja <robert.spralja@gmail.com>
2023-11-14 17:56:36 +0100

    feat: Quiz Model

--- Model/__init__.py
+++ Model/__init__.py
@@ -2,4 +2,6 @@ from .database import database
 
 from .dictionary_entry import DictionaryEntry
 
+from .quiz import Quiz
+
 from .translation_service import TranslationService, TranslationServiceError
---
+++ Model/quiz.py
@@ -0,0 +1,15 @@
+"""
+This is a Quiz class, it stores a question and 4 answers
+"""
+from typing import List
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Quiz:
+    question: str
+    answer: List[str]
+
+    def __post_init__(self):
+        if len(self.answer) != 4:
+            raise ValueError(f"answer List must be of length 4, not {len(self.answer)}")

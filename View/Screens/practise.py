34e98ab558183675640187d3d8c1c4d94a748236
Robert Spralja <robert.spralja@gmail.com>
2023-11-15 22:58:20 +0100

    refactor: moved colours to be members of class in practise view

--- View/Screens/practise.py
+++ View/Screens/practise.py
@@ -6,12 +6,13 @@ from Model import Quiz
 
 from Controller import PractiseController
 
-DEFAULT_BUTTON_COLOR = [1, 1, 1, 1]
-ANSWER_BUTTON_CORRECT_COLOR = (0, 1, 0, 1)
-ANSWER_BUTTON_INCORRECT_COLOR = (1, 0, 0, 1)
-
 
 class Practise(Screen):
+
+    DEFAULT_BUTTON_COLOR = [1, 1, 1, 1]
+    ANSWER_BUTTON_CORRECT_COLOR = [0, 1, 0, 1]
+    ANSWER_BUTTON_INCORRECT_COLOR = [1, 0, 0, 1]
+
     def __init__(self, **kwargs):
         self.next_button = None
         self.controller = PractiseController(self)
@@ -36,7 +37,7 @@ class Practise(Screen):
         self.ids.question.text = f"Which of the following is the best translation for: {quiz.question!r}"
 
         for answer_button, answer_text in zip(self.ids.answer_button_layout.children, quiz.answers):
-            answer_button.background_color = DEFAULT_BUTTON_COLOR
+            answer_button.background_color = self.DEFAULT_BUTTON_COLOR
             answer_button.text = answer_text
 
     def enable_next_button(self):
@@ -48,10 +49,10 @@ class Practise(Screen):
         self.ids.next_button.disabled = True
 
     def on_correct_answer(self, answer_button):
-        answer_button.background_color = ANSWER_BUTTON_CORRECT_COLOR
+        answer_button.background_color = self.ANSWER_BUTTON_CORRECT_COLOR
 
     def on_incorrect_answer(self, answer_button):
-        answer_button.background_color = ANSWER_BUTTON_INCORRECT_COLOR
+        answer_button.background_color = self.ANSWER_BUTTON_INCORRECT_COLOR
 
 
 Builder.load_file(os.path.join(os.path.dirname(__file__), "practise.kv"))

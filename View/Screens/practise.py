4cbf98c45dad5afb16ecc6bba953537f79d77b2c
Robert Spralja <robert.spralja@gmail.com>
2023-11-15 22:15:01 +0100

    feat: now button color reset after 'next' button is pressed

--- View/Screens/practise.py
+++ View/Screens/practise.py
@@ -7,6 +7,8 @@ from Model import Quiz
 
 from Controller import PractiseController
 
+DEFAULT_BUTTON_COLOR = [1, 1, 1, 1]
+
 
 class Practise(Screen):
     def __init__(self, **kwargs):
@@ -33,13 +35,11 @@ class Practise(Screen):
         self.ids.question.text = f"Which of the following is the best translation for: {quiz.question!r}"
 
         for answer_button, answer_text in zip(self.ids.answer_button_layout.children, quiz.answers):
+            answer_button.background_color = DEFAULT_BUTTON_COLOR
             answer_button.text = answer_text
 
     def answer(self, answer_button):
         self.controller.answer_button_on_press(answer_button)
-        quiz = self.controller.generate_quiz()
-
-        self.set_quiz(quiz)
 
     def enable_next_button(self):
         self.ids.next_button.opacity = 1

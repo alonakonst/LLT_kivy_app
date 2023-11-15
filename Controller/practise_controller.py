2608e898ba1f676c80444f0e1e54e707a90ac227
Robert Spralja <robert.spralja@gmail.com>
2023-11-15 22:08:50 +0100

    refactor: removed redundant code, related to previous commit

--- Controller/practise_controller.py
+++ Controller/practise_controller.py
@@ -38,7 +38,7 @@ class PractiseController:
 
         self.quiz_in_progress = False
 
-        self.view.create_next_button()
+        self.view.enable_next_button()
 
 
     def is_answer_correct(self, answer_text: str) -> bool:
@@ -69,6 +69,6 @@ class PractiseController:
         print("PRESS")
         self.view.set_quiz(self.current_quiz)
         self.correct_answer_index = 2
-        self.view.remove_next_button()
+        self.view.disable_next_button()
 
         self.quiz_in_progress = True

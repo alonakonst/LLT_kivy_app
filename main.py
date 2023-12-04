7d3272d26f498fe115dfd812e2621f9086c8acd0
Robert Spralja <robert.spralja@gmail.com>
2023-12-04 20:52:08 +0100

    fix: fixed the dummy database being created if all words are deleted

--- main.py
+++ main.py
@@ -17,7 +17,15 @@ class LLTApp(MDApp):
 
 
 if __name__ == '__main__':
+    dummy = False
+    # check if the database exists
+    from pathlib import Path
+    if not Path('database.db').exists():
+        dummy = True
+
     from Utility import initialise_database
-    # If dummy=True will fill the database with content if it's empty
-    initialise_database(dummy=True)
+
+    # If dummy==True will fill the database with content if it's empty
+    initialise_database(dummy=dummy)
+
     LLTApp().run()

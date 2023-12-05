54f5399d05c934c13637e172318f47683ae69fc3
Robert Spralja <robert.spralja@gmail.com>
2023-12-05 16:00:30 +0100

    feat: initising TranslationService in add_to_dict_controller

--- Controller/add_to_dict_controller.py
+++ Controller/add_to_dict_controller.py
@@ -1,8 +1,18 @@
 from Model import DictionaryEntry
 from Model import TranslationService, TranslationServiceError
 
+import os
+
+
 class AddToDictController:
 
+    def __init__(self):
+        api_key = os.environ.get('API_KEY')
+
+        if not api_key:
+            api_key = ''
+
+        self.translation_service = TranslationService(api_key=api_key)
 
     def add_entry(self, text, translation, notes):
         dictionary_entry = DictionaryEntry(text=text, translation=translation, notes=notes)
